5ece5b59ab1ea460552b2a1b4e42e0831cc5b28e
bijanbina <bijanbina@gmail.com>
2023-08-25 19:14:43 +0330

    Remove GUI Dependency

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,9 +1,9 @@
-#include <QGuiApplication>
+#include <QCoreApplication>
 #include "fa_chapar.h"
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QCoreApplication app(argc, argv);
 
     FaChapar *chapar = new FaChapar();
 
